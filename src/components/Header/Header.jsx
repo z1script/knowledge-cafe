45105f0cd76f97d940ecbf636c7baa1f5d1ad858
Zihadul Islam <zynscriptofficial@gmail.com>
2023-05-01 19:29:40 +0600

    header complete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Knowledge Cafe</h1>
+      <img src="/src/assets/profile.jpeg" alt="profile-picture" />
+    </div>
+  );
+};
+
+export default Header;
